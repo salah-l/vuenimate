8f39f0068a9b7964d1a1bcf8219b0eda06ba1755
salah <salah@louizy.com>
2023-12-16 22:37:26 +0100

    add index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import VaBase from "./VaBase.vue";
+import VaFadeInUp from "./VaFadeInUp.vue";
+import VaFadeInDown from "./VaFadeInDown.vue";
+import VaFadeInLeft from "./VaFadeInLeft.vue";
+import VaFadeInRight from "./VaFadeInRight.vue";
+
+export { VaBase, VaFadeInUp, VaFadeInDown, VaFadeInLeft, VaFadeInRight };
